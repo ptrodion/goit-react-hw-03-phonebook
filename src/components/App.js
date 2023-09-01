571d49629733e1133b8e2df1c26dd0d699f284ca
ptrodion <rdnpetrenko@gmail.com>
2023-09-01 22:02:18 +0100

    add everything

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,26 @@ import { ContactsList } from './ContactsList/ContactsList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedOurContacts = localStorage.getItem('contacts');
+    if (savedOurContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedOurContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount;
 
   addContact = newContact => {
     const isThisNameNotUnique = this.state.contacts.some(
